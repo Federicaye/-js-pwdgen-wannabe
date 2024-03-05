645fb40ad5c0517a4b0d6ed9089665eed9ab28e5
Federicaye <gallofederica58@gmail.com>
2024-03-05 14:53:24 +0100

    .

--- prova.js
+++ prova.js
@@ -4,4 +4,4 @@ let secondName = prompt('qual è il tuo cognome?');
 
 let color = prompt('qual è il tuo colore preferito?');
 
-document.getElementById("NameColor").innerHTML= username + secondName + color;
+document.getElementById("NameColor").innerHTML= username + secondName + color + 23;
